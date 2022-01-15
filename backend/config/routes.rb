e276ac4f698b1ce05d201d12fb5f36ea42c88d5f
Mateus Sartori <mateus.sartori@credisis.com.br>
2022-01-15 18:47:46 -0400

    fix: fix resource routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json } do
-    resource :github, except: %i[show update], controller: :github do
+    resources :github, except: %i[show update], controller: :github do
       collection do
-        get '/', action: :index
         get 'repositories', action: :repositories_from_github
       end
     end
