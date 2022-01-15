5709920fcc3c2abdbc09639cc7a9174456b8ce1f
Mateus Sartori <mateus.sartori@credisis.com.br>
2022-01-15 18:43:24 -0400

    refactor: remove exception destroy from routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json } do
-    resource :github, except: %i[show update destroy], controller: :github do
+    resource :github, except: %i[show update], controller: :github do
       collection do
         get '/', action: :index
         get 'repositories', action: :repositories_from_github
