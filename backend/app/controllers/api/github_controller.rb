49ae4c8e53d5e69dfe10f5685f308a9c148e2ce0
Mateus Sartori <mateus.sartori@credisis.com.br>
2022-01-15 18:41:53 -0400

    feat: add destroy function and refactor

--- backend/app/controllers/api/github_controller.rb
+++ backend/app/controllers/api/github_controller.rb
@@ -1,5 +1,6 @@
 class Api::GithubController < ApplicationController
   include Repository
+  before_action :set_featured_repositories, only: %i[destroy]
 
   def create
     @repository = FeaturedRepository.new(repository_params)
@@ -12,14 +13,28 @@ class Api::GithubController < ApplicationController
   end
 
   def index
-    render json: { featured_repositories: featured_repositories }
+    render json: { featured_repositories: FeaturedRepository.all }
   end
 
+  def destroy
+    if @featured_repositories.destroy
+      render status: :no_content
+    else
+      render json: @featured_repositories.errors, status: :unprocessable_entity
+    end
+  end
+
+  # Get repositories from Github API Service
   def repositories_from_github
-    render json: { repositories: repositories_list(params[:language]) }
+    github_query = extract_repositories_info(params[:language])
+    render json: { repositories: github_query[:body] }, status: github_query[:status]
   end
 
   private
+
+  def set_featured_repositories
+    @featured_repositories = FeaturedRepository.find(params[:id])
+  end
 
   def repository_params
     params.fetch(:repository, {}).permit(:id_from_repository, :owner, :name, :description,
